957799a22b900518d2219a106f7e2d2455dbede0
ge.junges@gmail.com <ge.junges@gmail.com>
2018-09-03 02:02:38 +0200

    finishing data structure algorithm

--- Algorithms.UnitTests/DataStructure/TreeDataStructuresTests.cs
+++ Algorithms.UnitTests/DataStructure/TreeDataStructuresTests.cs
@@ -1,9 +1,9 @@
-﻿using Algorithms.BinaryTree;
+﻿using Algorithms.DataStructure;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 
-namespace Algorithms.UnitTests.Tree {
+namespace Algorithms.UnitTests.DataStructure {
     public class TreeDataStructuresTests {
 
         private TreeDataStructures _treeObjectsNode;
@@ -13,18 +13,25 @@ namespace Algorithms.UnitTests.Tree {
             _treeObjectsNode = new TreeDataStructures();
         }
 
-        [TestCaseSource(nameof(GetTestCase))]
-        public void Test_tree(List<NodesData> data, int node, Node expected) {
-             _treeObjectsNode.RetrieveFullTree(node, data);
+        [Test]
+        public void RetrieveFullData_should_retrieve_all_nodes() {
+            var data = CreateData();
+            var expected = CreateExpected();
+            var actual = _treeObjectsNode.RetrieveFullData(data);
+
+            Assert.AreEqual(expected, actual);
         }
 
-        private static IEnumerable<object> GetTestCase() {
-            yield return new TestCaseData(CreateData(), 1, CreateExpectedNode1());
-            yield return new TestCaseData(CreateData(), 2, CreateExpectedNode2());
-            yield return new TestCaseData(CreateData(), 7, CreateExpectedNode7());
+        private Node[] CreateExpected() {
+
+            return new Node[] {
+                CreateExpectedNode1(),
+                CreateExpectedNode2(),
+                CreateExpectedNode7(),
+            };
         }
 
-        private static object CreateExpectedNode1() {
+        private static Node CreateExpectedNode1() {
             return new Node {
                 Id = 1,
                 LinkedNodes = new Node[] {
@@ -64,7 +71,7 @@ namespace Algorithms.UnitTests.Tree {
             };
         }
 
-        private static object CreateExpectedNode2() {
+        private static Node CreateExpectedNode2() {
             return new Node {
                 Id = 2,
                 LinkedNodes = new Node[] {
@@ -104,7 +111,7 @@ namespace Algorithms.UnitTests.Tree {
             };
         }
 
-        private static object CreateExpectedNode7() {
+        private static Node CreateExpectedNode7() {
             return new Node {
                 Id = 7,
                 LinkedNodes = new Node[] {
@@ -121,17 +128,17 @@ namespace Algorithms.UnitTests.Tree {
             };
         }
 
-        private static List<NodesData> CreateData() {
-            return new List<NodesData> {
-                new NodesData { Id = new Guid("2CD83141-08A0-4B91-AB5B-B3D2A4A7007B"), FromNodeId = 1, ToNodeId = 3 },
-                new NodesData { Id = new Guid("1BB30262-1E26-4D78-A6D3-4C01BEEA63A1"), FromNodeId = 2, ToNodeId = 3 },
-                new NodesData { Id = new Guid("AB2A2377-7537-4703-AA89-F80672B56D20"), FromNodeId = 3, ToNodeId = 4 },
-                new NodesData { Id = new Guid("03667173-2BC8-49AA-89FA-582E22ABA4EF"), FromNodeId = 4, ToNodeId = 8 },
-                new NodesData { Id = new Guid("4E51CA97-D226-437C-B891-8C24B1AC984C"), FromNodeId = 5, ToNodeId = 8 },
-                new NodesData { Id = new Guid("5CD47D9D-EB88-4027-B2FC-DE10B148064B"), FromNodeId = 6, ToNodeId = 8 },
-                new NodesData { Id = new Guid("2CF33F2E-1BE2-4EA2-BC4C-5F5B39B353BB"), FromNodeId = 7, ToNodeId = 6 },
-                new NodesData { Id = new Guid("FF81B02D-4E72-4A33-9FBC-9D4E3C5998F1"), FromNodeId = 3, ToNodeId = 5 },
-                new NodesData { Id = new Guid("CBBD7C22-A328-4147-BD00-9C49B79DCDD0"), FromNodeId = 3, ToNodeId = 6 },
+        private static List<Entity> CreateData() {
+            return new List<Entity> {
+                new Entity { Id = new Guid("2CD83141-08A0-4B91-AB5B-B3D2A4A7007B"), FromNodeId = 1, ToNodeId = 3 },
+                new Entity { Id = new Guid("1BB30262-1E26-4D78-A6D3-4C01BEEA63A1"), FromNodeId = 2, ToNodeId = 3 },
+                new Entity { Id = new Guid("AB2A2377-7537-4703-AA89-F80672B56D20"), FromNodeId = 3, ToNodeId = 4 },
+                new Entity { Id = new Guid("03667173-2BC8-49AA-89FA-582E22ABA4EF"), FromNodeId = 4, ToNodeId = 8 },
+                new Entity { Id = new Guid("4E51CA97-D226-437C-B891-8C24B1AC984C"), FromNodeId = 5, ToNodeId = 8 },
+                new Entity { Id = new Guid("5CD47D9D-EB88-4027-B2FC-DE10B148064B"), FromNodeId = 6, ToNodeId = 8 },
+                new Entity { Id = new Guid("2CF33F2E-1BE2-4EA2-BC4C-5F5B39B353BB"), FromNodeId = 7, ToNodeId = 6 },
+                new Entity { Id = new Guid("FF81B02D-4E72-4A33-9FBC-9D4E3C5998F1"), FromNodeId = 3, ToNodeId = 5 },
+                new Entity { Id = new Guid("CBBD7C22-A328-4147-BD00-9C49B79DCDD0"), FromNodeId = 3, ToNodeId = 6 },
             };
         }
     }
---
+++ Algorithms/DataStructure/TreeDataStructures.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.DataStructure {
+    public class TreeDataStructures {
+
+        public Node[] RetrieveFullData(List<Entity> EntityList) {
+
+            var childrenIds = from child in EntityList
+                              select child.ToNodeId;
+
+            var roots = from root in EntityList
+                        where !childrenIds.Contains(root.FromNodeId)
+                        select root;
+
+            var dic = new Dictionary<int, List<int>>();
+
+            EntityList.ForEach(e => {
+                if (dic.ContainsKey(e.FromNodeId)) {
+                    dic[e.FromNodeId].Add(e.ToNodeId);
+                } else {
+                    dic.Add(e.FromNodeId, new List<int> { e.ToNodeId });
+                }
+            });
+
+            var lista = new List<Node>();
+            foreach (var root in roots) {
+                lista.Add(
+                   new Node {
+                       Id = root.FromNodeId,
+                       LinkedNodes = CrieNodes(dic[root.FromNodeId], dic)
+                   });
+            }
+
+            return lista.ToArray();
+        }
+
+        private Node[] CrieNodes(List<int> list, Dictionary<int, List<int>> dic) {
+            var listona = new List<Node>();
+
+            foreach (var item in list) {
+                if (item >= dic.Count) {
+                    listona.Add(new Node { Id = item });
+                    continue;
+                }
+
+                listona.Add(
+                    new Node {
+                        Id = item,
+                        LinkedNodes = CrieNodes(dic[item], dic)
+
+                    });
+            }
+
+            return listona.ToArray();
+        }
+
+    }
+
+    public class Entity {
+        public Guid Id { get; set; }
+        public int FromNodeId { get; set; }
+        public int ToNodeId { get; set; }
+    }
+
+    public class Node {
+        public int Id { get; set; }
+        public Node[] LinkedNodes { get; set; }
+
+        public override bool Equals(object obj) {
+            if (obj == null || !(obj is Node)) {
+                return false;
+            }
+
+            var other = obj as Node;
+
+            return other.Id == Id
+                && other.LinkedNodes.Count() == LinkedNodes.Count();
+        }
+
+        public override int GetHashCode() {
+            return Id.GetHashCode() ^ LinkedNodes.GetHashCode();
+        }
+    }
+}
